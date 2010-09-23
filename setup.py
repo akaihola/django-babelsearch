a0cd4d33acad0d182fc1d425df423fce62323ba2
Antti Kaihola <akaihol+django@ambitone.com>
2010-09-23 14:28:02 +0300

    Bumped version number to 0.2dev. Added templates to distribution.

--- setup.py
+++ setup.py
@@ -2,15 +2,19 @@ from setuptools import setup
 
 setup(
     name = 'django-babelsearch',
-    version = '0.1',
-    packages = ['babelsearch'],
+    version = '0.2dev',
+    packages = ['babelsearch',
+                'babelsearch.migrations',
+                'babelsearch.management',
+                'babelsearch.management.commands'],
+    package_data={'babelsearch': ['templates/babelsearch/*.html']},
     author = 'Antti Kaihola',
     author_email = 'akaihol+django@ambitone.com',
     description = ('Indexing/search re-usable app for Django '
                    'featuring multi-language capabilities'),
     url = 'http://github.com/akaihola/django-babelsearch/tree/master',
     download_url = ('http://www.github.com/akaihola/django-babelsearch/'
-                    'tarball/0.1'),
+                    'tarball/0.2'),
     classifiers=(
         'Development Status :: 3 - Alpha',
         'Framework :: Django',
